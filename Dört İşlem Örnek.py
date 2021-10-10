20dd1f3a0a9581b620ee40d9b0bbd33c902a1fc7
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-10 15:57:48 +0300

    Add files via upload

---
+++ Dört İşlem Örnek.py
@@ -0,0 +1,17 @@
+a =  int (input ("sayi 1 "))
+b =  int (input("sayi 2 "))
+
+seçenek = int (input("\n 1-Topla \n 2-Çıkar \n 3-Çarp \n 4-Böl \n"))
+
+if seçenek==1:
+    print(a+b)
+
+elif seçenek==2:
+    print(a-b)
+
+elif seçenek==3:
+    print(a*b)
+
+elif seçenek==4:
+    print(a/b)
+
