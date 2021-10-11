34aa1a92b14707e55e639eba5591893778279fcb
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-11 13:08:08 +0300

    Add files via upload

---
+++ Print Fonksiyon Özellikleri.py
@@ -0,0 +1,13 @@
+a=5
+b=3
+c="Tahsin"
+d= "Taydaş"
+#print(a+b) #Matematik işlemleri yapılabilir
+#print(*c)  #Kelime ayırmak için kullanılabilir.
+#print(a,c) #2 ayrı değşkeni "," yardımıyla birleştirir.
+#print("Sayın {} {} Hoşgeldiniz".format(c,d)) #String yapısında "format" kullanarak değişkenleri atayabiliriz.
+#print(("Sayın %s %s Hoşgeldiniz" % (c,d))) #String yapısında "%s-%" yapısını kullanarak değişken atayabiliriz.
+
+print(a,end=" X ") #a değişkenin sonuna " X " işaretini koyarak yazdırdı.
+print(b)
+
