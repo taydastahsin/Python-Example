58d146076dfcf2d977e1bb947fc2b653f4f11a15
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-13 09:49:00 +0300

    Add files via upload

---
+++ List Değişkeni.py
@@ -0,0 +1,18 @@
+# list() ya da [] şeklinde belirtebiliriz.
+a=["Ali","Veli","Ahmet","Mehmet","1","2","3"]
+# print(a[5]) # "a" listesinin değişkenlerini gösteriyor.
+# print(len(a)) # "a" listesinin uzunluğunu belirtiyor.
+# print(a.pop()) # "a" listesinin son elemanını gösterir.
+# a.remove("1") #"a" listesinin "1" elemanını sildi.
+# print(a[0:7:2]) # "a" listesinin elemanlarını istediğimiz gibi sıraladık.
+# a.append("4") # "a" listesine "4" elemanını ekledik.
+# print(type(a)) # "a" listesinin türünü gösterir.
+# a[2]="Tahsin" # "a" listesinin "Ahmet" elemanını "Tahsin" oalrak değiştirdik.
+# a.sort() # "a" listesinin küçükten-büyüğe olarak sıraladık.
+# a.sort(reverse= True) # "a" listesinin büyükten-küçüğe sıraladık.
+print(a)
+
+
+
+
+
