dbbc41fd13edf8429b4a123dfa123d397db9da4c
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-14 23:36:30 +0300

    Add files via upload

---
+++ Hesap Makinesi(input).py
@@ -0,0 +1,9 @@
+sayi1 = float (input("Birinci sayıyı yazınız :"))
+sayi2 = float(input("İkinci sayıyı yazınız :"))
+
+print("""
+Toplama : {}
+Çıkarma : {}
+Çarpma : {}
+Bölme : {}""".format(sayi1+sayi2,sayi1-sayi2,sayi1*sayi2,sayi1/sayi2))
+
