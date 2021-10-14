afce560806e939ef7d63cb80a7b6cace5510c27c
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-14 11:21:07 +0300

    Add files via upload

---
+++ Tuple Veri Özellikleri.py
@@ -0,0 +1,7 @@
+# a=() # Tuple gösterim şekli "()"
+# b=tuple() #Tuple gösterim şekli
+a=(1,15,23,89,566,"Tahsin","Ayşe","Serdar",1,"Tahsin",15,1,100)
+
+print(a.count("Tahsin")) # "Tahsin" adlı kaç eleman olduğunu gösterir.
+
+print(a.index("Serdar")) # "Serdar" elemanın kaç index olduğunu (harften) gösterir.
