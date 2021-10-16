c191fef71bec8b5eb4d885f14dada2c652367878
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-16 16:38:46 +0300

    Add files via upload

---
+++ Mantıksal Operatörler.py
@@ -0,0 +1,9 @@
+a= 1 # "bool" tooluyla değerlendirildiğinde "True" gösterir.
+b= 0 # "bool" tooluyla değerlendirildiğinde "False" gösterir.
+# print(bool(a))
+# print(bool(b))
+print(type(b == a)) # a ve b değişkenin birbirine eşitse "True" döndürmesini söyledik.
+print(b != a) # a ve b değişkenin biribirine eşitse "False" döndürmesini söyledik.
+print(a > b)  # a değişkenin b den büyükse "True" döndürmesini söyledik.
+print(a < b)  # a değişkenin b den küçükse "True" döndürmesini söyledik.
+print(a >= b) # a değişkenin b den büyükse yada eşitse "True" döndürmesini söyledik.
