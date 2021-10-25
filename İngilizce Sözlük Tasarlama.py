063d95696c851bcbd9dfa59e597c1071debf2be7
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-10-25 21:38:37 +0300

    Add files via upload

---
+++ İngilizce Sözlük Tasarlama.py
@@ -0,0 +1,19 @@
+sözlük = dict()
+
+ing = input("İngilizce bir kelime yazın : ")
+tr = input("Türkçe bir kelime yazın : ")
+sözlük[ing]=tr
+ing = input("İngilizce bir kelime yazın : ")
+tr = input("Türkçe bir kelime yazın : ")
+sözlük[ing]=tr
+ing = input("İngilizce bir kelime yazın : ")
+tr = input("Türkçe bir kelime yazın : ")
+sözlük[ing]=tr
+ing = input("İngilizce bir kelime yazın : ")
+tr = input("Türkçe bir kelime yazın : ")
+sözlük[ing]=tr
+ing = input("İngilizce bir kelime yazın : ")
+tr = input("Türkçe bir kelime yazın : ")
+sözlük[ing]=tr
+print(sözlük)
+
