e708a214b63815b7079786d79c1a1758698cc846
taydastahsin <56533800+taydastahsin@users.noreply.github.com>
2021-12-08 10:58:14 +0300

    Add files via upload

--- PyQt5 QStacked Widget Özellikleri/ders15.py
+++ PyQt5 QStacked Widget Özellikleri/ders15.py
@@ -6,29 +6,29 @@ class ders15_main (QMainWindow):
         super().__init__()
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
-        self.home_page_index = 0
-        self.kitap_listesi_index = 1
-        self.kitap_ekle_index = 2
-        self.uye_listesi_index = 3
-        self.uye_ekle_index = 4
+        self.kitap_ekle_index  = 0
+        self.uye_ekle_index  = 1
+        self.uye_listesi_index = 2
+        self.home_page_index = 3
+        self.kitap_listesi_index = 4
 
         self.ui.actionAna_Sayfa.triggered.connect(self.go_home_page)
         self.ui.actionKitap_Listesi.triggered.connect(self.go_kitap_listesi)
         self.ui.actionKitap_Ekle.triggered.connect(self.go_kitap_ekle)
-        self.ui.action_ye_Listesi.triggered.connect(self.go_üye_listesi)
+        self.ui.action_ye_Listesi.triggered.connect(self.go_uye_listesi)
         self.ui.action_ye_Ekle.triggered.connect(self.go_uye_ekle)
 
 
-        def go_home_page(self):
-            self.ui.stackedWidget.setCurrentIndex(self.home_page_index)
-        def go_kitap_listesi(self):
-            self.ui.stackedWidget.setCurrentIndex(self.kitap_listesi_index)
-        def go_kitap_ekle(self):
-            self.ui.stackedWidget.setCurrentIndex(self.kitap_ekle_index)
-        def go_üye_listesi(self):
-            self.ui.stackedWidget.setCurrentIndex(self.uye_listesi_index)
-        def go_uye_ekle(self):
-            self.ui.stackedWidget.setCurrentIndex(self.uye_ekle_index)
+    def go_home_page(self):
+        self.ui.stackedWidget.setCurrentIndex(self.home_page_index)
+    def go_kitap_listesi(self):
+        self.ui.stackedWidget.setCurrentIndex(self.kitap_listesi_index)
+    def go_kitap_ekle(self):
+        self.ui.stackedWidget.setCurrentIndex(self.kitap_ekle_index)
+    def go_uye_listesi(self):
+        self.ui.stackedWidget.setCurrentIndex(self.uye_listesi_index)
+    def go_uye_ekle(self):
+        self.ui.stackedWidget.setCurrentIndex(self.uye_ekle_index)
 
 
 
